b084c7bf7c033c98d215d9bb09a4c25734699305
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-11-11 23:14:16 +0200

    update throttle and debounce

--- throttle-debounce.js
+++ throttle-debounce.js
@@ -18,5 +18,37 @@ refs.button.addEventListener("click", handleClick);
 refs.input1.focus();
 refs.button.click();
 
+// function throttle(callback, delay) {
+//   let id = 0;
+
+//   return function (e) {
+//     if (id) return;
+
+//     id = setTimeout(() => {
+//       callback(e);
+//       clearTimeout(id);
+//       id = 0;
+//     }, delay);
+//   };
+// }
+
 // refs.input1.addEventListener("input", _.throttle(handleInput, 300));
+
+function debounce(callback, delay) {
+  let id = 0;
+
+  return function (e) {
+    if (id) {
+      clearTimaout(id);
+      id = 0;
+    }
+
+    id = setTimeout(() => {
+      callback(e);
+      clearTimeout(id);
+      id = 0;
+    }, delay);
+  };
+}
+
 refs.input1.addEventListener("input", _.debounce(handleInput, 300));
